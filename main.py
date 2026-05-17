d59e92f87e1e2abb93a68eeadcc36e91d9e2acc5
Andrew P <pyea0230@gmail.com>
2026-05-17 15:53:34 +1000

    Added loading for lda, nsw and qld data

--- main.py
+++ main.py
@@ -11,5 +11,14 @@ transmission_lines_df = pd.read_csv("data/power_stations.csv")
 transmission_lines_df.index = transmission_lines_df["objectid"]
 transmission_lines_df.drop("objectid", axis=1, inplace=True)
 
-x = geo_pd.read_file("data/ASGS_2021_MAIN_STRUCTURE/ASGS_2021_Main_Structure_GDA2020.gpkg")
-print(x.head())
+lga_pd = geo_pd.read_file("data/lga.shp")
+
+nsw_electricity_consumption = pd.read_csv("data/nsw_average_electricity_consumption_by_lga.csv")
+
+nsw_electricity_consumption.index = nsw_electricity_consumption["Local Government Area"]
+nsw_electricity_consumption.drop("Local Government Area")
+
+qld_electricity_consumption = pd.read_csv("data/qld_average_electricity_consumption_by_lga.csv")
+
+qld_electricity_consumption.index = qld_electricity_consumption["Local Government Area"]
+qld_electricity_consumption.drop("Local Government Area")
